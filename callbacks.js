a5a16142313505e70150efd0679de3d383e189ab
ferrazf <ferrazf9@gmail.com>
2019-04-05 00:27:26 +0000

    Adding callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,17 +1,48 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
-var wLength = words.map(function(word){ 
-      return word.length; 
-});
-
-var wUpper = words.map(function(word){ 
-      return word.toUpperCase();
-});
-
 var wWords = words.map(function(word){ 
     return word.split('').reverse().join('');
 });
 
-console.log(wLength);
-console.log(wUpper);
-console.log(wWords);
+function getLength(arr, action) {
+      var wLen = [];
+      arr.map(function(word){
+            wLen.push(word.length);
+      });
+      return action(wLen); 
+}
+
+function lenCbFunc (lengthArr)
+{
+      console.log(lengthArr);
+}
+
+function getUpper(arr, action) {
+      var wUpp = [];
+      arr.map(function(word){
+            wUpp.push(word.toUpperCase());
+      });
+      return action(wUpp); 
+}
+
+function uppCbFunc(upperArr)
+{
+      console.log(upperArr);
+}
+
+function getSplit(arr, action) {
+      var wSplit = [];
+      arr.map(function(word){
+            wSplit.push(word.split('').reverse().join(''));
+      });
+      return action(wSplit); 
+}
+
+function splitCbFunc(splitArr)
+{
+      console.log(splitArr);
+}
+
+getLength(words, lenCbFunc);
+getUpper(words, uppCbFunc);
+getSplit(words, splitCbFunc);
